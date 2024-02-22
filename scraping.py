509ea8834b8886e6d12995c299b9bf7dd2ce3d98
astra2001 <wonderland0120@gmail.com>
2024-02-22 21:13:27 +0900

    kaeta

--- scraping.py
+++ scraping.py
@@ -11,36 +11,46 @@ def ReadCSV(path):
 def GetUrl():
     pass
 
+def Getinfo(ins):
+    # Find all graduate school entries in the provided HTML
+    graduate_school_entries = ins.select('.pt-cv-content-item')
+
+    # Parse and print details of each graduate school
+    for entry in graduate_school_entries:
+        # Extracting the graduate school name
+        name = entry.find('h4', class_='pt-cv-title').get_text(strip=True)
+        
+        # Extracting the research departments
+        departments = entry.select('.pt-cv-ctf-list .pt-cv-ctf-column .pt-cv-ctf-value')
+        departments_list = [dept.get_text(strip=True) for dept in departments]
+        
+        # Extracting the location (prefecture)
+        location = entry.find('div', class_='pt-cv-meta-fields').get_text(strip=True)
+
+    return name, entry.find('a')['href'], ', '.join(departments_list), location
+
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("-u", "--urldic", required=True)
-    # parser.add_argument("-o", "--output", help="output path")
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--urldic", default="universities.csv")
+    parser.add_argument("-o", "--output", help="output path")
+    args = parser.parse_args()
 
-    # url_dic = args.urldic
-    # url_list = ReadCSV(url_dic)
-
-    url_list = [["放送大学大学院","https://univ-journal.jp/daigakuin/73529/"]]
+    url_dic = args.urldic
+    url_list = ReadCSV(url_dic)
 
 
     for data in url_list:
         univ_name = data[0]
         url = data[1]
+        univ_place = data[-1]
+
 
         res = requests.get(url)
         soup = BS(res.text, "html.parser")
-        name = soup.find_all("div", {"class","post-single-content box mark-links"})[0]
-        table = name.find_all("table")[0]
-        td_ = table.find_all("td")
-        td_list = []
-        for td in td_:
-            text = td.get_text(separator="<br/>")
-            element = text.split("<br/>")
-            td_list.append(element)
-        print(td_list)
+
+        name, , 
                     
-print("hello")
 
 if __name__ == "__main__":
     main()
